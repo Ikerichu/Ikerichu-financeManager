e25b13614f763e88cfa1c29e667918f9662e440a
Iker Moya <92671501+Ikerichu@users.noreply.github.com>
2026-03-16 09:16:00 +0000

    classTransaction

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Float, Date, ForeignKey
+from sqlalchemy import String, Float, Date, ForeignKey, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 from datetime import date
@@ -31,3 +31,21 @@ class Category(db.Model):
     name: Mapped[str] = mapped_column(String(50), nullable=False)
 
     transactions: Mapped[List["Transaction"]] = relationship(back_populates="category")
+
+class Transaction(db.Model):
+    __tablename__ = "transactions"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    amount: Mapped[float] = mapped_column(Float, nullable=False)
+    description: Mapped[str] = mapped_column(String(255))
+
+    type: Mapped[str] = mapped_column(Enum("income", "expense", name="transaction_type"), String(10), nullable=False)
+
+    date: Mapped[date] = mapped_column(Date)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+
+    user: Mapped["User"] = relationship(back_populates="transactions")
+    category: Mapped["Category"] = relationship(back_populates="transactions")
