94d64f8983a22b455ce7a39b044de0a461e24064
Iker Moya <92671501+Ikerichu@users.noreply.github.com>
2026-03-17 12:19:26 +0000

    fetch&functiosHome

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,52 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
+  const [transactions, setTransactions] = useState([]);
 
-	const { store, dispatch } = useGlobalReducer()
+  useEffect(() => {
+    fetch(`${VITE_BACKEND_URL}/transactions`)
+      .then(res => res.json())
+      .then(data => setTransactions(data))
+      .catch(err => console.error(err));
+  }, []);
 
-	const loadMessage = async () => {
-		try {
-			const backendUrl = import.meta.env.VITE_BACKEND_URL
+  const income = transactions
+    .filter(t => t.type === "income")
+    .reduce((acc, t) => acc + t.amount, 0);
 
-			if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
+  const expense = transactions
+    .filter(t => t.type === "expense")
+    .reduce((acc, t) => acc + t.amount, 0);
 
-			const response = await fetch(backendUrl + "/api/hello")
-			const data = await response.json()
+  const balance = income - expense;
 
-			if (response.ok) dispatch({ type: "set_hello", payload: data.message })
-
-			return data
-
-		} catch (error) {
-			if (error.message) throw new Error(
-				`Could not fetch the message from the backend.
-				Please check if the backend is running and the backend port is public.`
-			);
-		}
-
-	}
-
-	useEffect(() => {
-		loadMessage()
-	}, [])
-
-	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
-			</div>
-		</div>
-	);
-}; 
+  
