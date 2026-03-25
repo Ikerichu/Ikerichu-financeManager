4300cba9e0bb319c01ff3752c1283ee8f7c06fdb
Iker Moya <92671501+Ikerichu@users.noreply.github.com>
2026-03-25 13:40:39 +0000

    ruta profile

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,8 +7,7 @@ import {
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
+import { Profile } from "./pages/Profile";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,6 +22,7 @@ export const router = createBrowserRouter(
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
+        <Route path= "/profile" element={<Profile />} />
       </Route>
     )
 );
